86f253dd3df9ae7c30153b5f0982f2e202ba4e24
paparao.pandiri <paparao.pandiri@affiliate.ansr.com>
2023-10-23 10:05:55 +0530

    first commit

---
+++ src/main/java/com/ms/managementsystemeks/ManagementSystemEksApplication.java
@@ -0,0 +1,13 @@
+package com.ms.managementsystemeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementSystemEksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementSystemEksApplication.class, args);
+	}
+
+}
